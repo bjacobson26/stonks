98a14ce1afe30ccc6934ea186b70d3bd37a75444
Bo Jacobson <bjacobson26@gmail.com>
2020-04-01 21:55:42 -0700

    change method name for getQuote

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.stonks', async () => {
+	let getQuote = vscode.commands.registerCommand('extension.stonks', async () => {
 		// The code you place here will be executed every time your command is executed
 		let options: vscode.InputBoxOptions = {
 			prompt: "Enter a stock ticker"
@@ -35,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(getQuote);
 }
 
 // this method is called when your extension is deactivated
