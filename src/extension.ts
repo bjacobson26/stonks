e0f32e493222623b71ad783bccd449493935f393
Bo Jacobson <bjacobson26@gmail.com>
2020-04-01 23:31:48 -0700

    add percentage change to GetQuote

--- src/extension.ts
+++ src/extension.ts
@@ -8,14 +8,7 @@ const yahooFinance: any = require('yahoo-finance');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "stonks" is now active!');
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let getQuote = vscode.commands.registerCommand('extension.stonks', async () => {
+	let getQuote = vscode.commands.registerCommand('extension.stonksGetQuote', async () => {
 		// The code you place here will be executed every time your command is executed
 		let options: vscode.InputBoxOptions = {
 			prompt: "Enter a stock ticker"
@@ -30,7 +23,8 @@ export function activate(context: vscode.ExtensionContext) {
 			if(err) {
 				vscode.window.showInformationMessage(`Could not retrieve price for ${input}`);
 			} else {
-		        vscode.window.showInformationMessage(`${_.toUpper(input)} $${quotes.price.regularMarketPrice}`);
+				let percentChange = (quotes.price.regularMarketChangePercent * 100).toFixed(2);
+		        vscode.window.showInformationMessage(`${_.toUpper(input)} $${quotes.price.regularMarketPrice} (${percentChange}%)`);
 			}
 		});
 	});
